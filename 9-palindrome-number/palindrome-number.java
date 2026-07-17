22696f7b0320031471724df5119d77988ecb6f65
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-07-17 15:15:55 +0530

    Time: 5 ms (80.71%) | Memory: 45.9 MB (53.81%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,25 +1,26 @@
 import java.util.Scanner;
-class Solution {
-    public static boolean isPalindrome(int x) {
-        if(x<0)
-        return false;
+class Solution{
+    public static boolean isPalindrome(int x){
 
-        int original = x;
+        if(x<0) return false;
+
+        int original=x;
         int reverse=0;
         while(x!=0){
-           int digit=x%10;
+            int digit=x%10;
             reverse=reverse*10+digit;
             x=x/10;
         }
         return original==reverse;
     }
-public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    int n=sc.nextInt();
-    if(isPalindrome(n)){
-        System.out.print("Palindrome");
-    }else{
-        System.out.print("Not Palindrome");
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(isPalindrome(n)){
+            System.out.print("Palindrome");
+        }
+        else{
+            System.out.print("Not Palindome");
+        }
     }
-}
 }
