5c62b86a9f9fefbd1235da1ea379f595fb009ca3
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-07-18 12:13:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (33.58%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -9,8 +9,8 @@ class Solution {
 
         int first=0;
         int second=1;
-
-        for(int i=2;i<=n;i++){
+        for(int i=2;i<=n;i++)
+        {
             int third=first+second;
             first=second;
             second=third;
