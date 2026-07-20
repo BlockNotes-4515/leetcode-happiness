b40438cb3656617a84a54854339e725a2081efea
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-07-20 11:29:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (62.83%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -5,7 +5,7 @@ class Solution {
         int j=n-1;
         int k=(m+n)-1;
 
-        //Merging the two Sorted arrays.
+        //Merging of Two sorted arrays.
         while(i>=0 && j>=0){
             if(nums1[i]>nums2[j]){
                 nums1[k]=nums1[i];
@@ -17,6 +17,7 @@ class Solution {
             }
             k--;
         }
+        //Possibility with the second array.
         while(j>=0){
             nums1[k]=nums2[j];
             j--;
