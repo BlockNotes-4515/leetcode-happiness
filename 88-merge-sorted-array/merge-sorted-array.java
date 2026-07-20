9ed6db410eef62b8ff41d8b0d0abd64218c2b280
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-07-20 11:35:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (89.33%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,25 +1,27 @@
-class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+class Solution{
+    public void merge(int A[], int m, int B[], int n){
 
+        //Declaration of the variables.
         int i=m-1;
         int j=n-1;
         int k=(m+n)-1;
+        //int C[]=new int[];
 
-        //Merging of Two sorted arrays.
+        //Merging of the two sorted arrays.
         while(i>=0 && j>=0){
-            if(nums1[i]>nums2[j]){
-                nums1[k]=nums1[i];
+            if(A[i]>B[j]){
+                A[k]=A[i];
                 i--;
             }
             else{
-                nums1[k]=nums2[j];
+                A[k]=B[j];
                 j--;
             }
             k--;
         }
-        //Possibility with the second array.
+        //Possibility of large array merging.
         while(j>=0){
-            nums1[k]=nums2[j];
+            A[k]=B[j];
             j--;
             k--;
         }
