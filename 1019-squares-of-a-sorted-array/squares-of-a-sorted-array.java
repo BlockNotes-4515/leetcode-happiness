74e011c9c015f6abcd148e3bcc17817b65e97060
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-07-23 17:55:18 +0530

    Time: 10 ms (36.78%) | Memory: 49.1 MB (5.01%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -4,8 +4,11 @@ class Solution {
         int n=nums.length;
         int result[]=new int[n];
         for(int i=0;i<n;i++){
+
+            //Squaring of all items in an array.
             result[i]=nums[i]*nums[i];
         }
+        //Sort the array simply.
         Arrays.sort(result);
         return result;
     }
