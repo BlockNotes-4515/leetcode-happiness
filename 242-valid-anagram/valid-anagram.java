2a48c420597f4d9dbd614ece33ed44da6ea7274f
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-07-31 18:00:10 +0530

    Time: 2 ms (99.62%) | Memory: 44.7 MB (57.02%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,16 +1,38 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
+        //First Approach
+        /*
         if(s.length()!=t.length()){
             return false;
         }
-        char ch1[]=s.toCharArray();
-        char ch2[]=t.toCharArray();
 
-        //Sorting an char array
+        char[] ch1=s.toCharArray();
+        char[] ch2=t.toCharArray();
+
+        //Sorting an arrays here
         Arrays.sort(ch1);
         Arrays.sort(ch2);
 
+        //Returning the equal word of characters they have
         return Arrays.equals(ch1,ch2);
+        */
+        // Second Approach here given
+        int freq[]=new int[26];
+
+        for(char c:s.toCharArray()){
+            int indx=c-'a';
+            freq[indx]=freq[indx]+1;
+        }
+        for(char c:t.toCharArray()){
+            int indx=c-'a';
+            freq[indx]=freq[indx]-1;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+        return true;
     }
 }
