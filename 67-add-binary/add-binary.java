519ba3a898481884f55658a8ab44a68fda47a7b1
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-07-31 11:01:40 +0530

    Time: 2 ms (47.08%) | Memory: 43.6 MB (53.90%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,8 +1,7 @@
 class Solution {
     public String addBinary(String a, String b) {
-        
-        StringBuilder ans = new StringBuilder();
 
+        StringBuilder ans=new StringBuilder();
         int i=a.length()-1;
         int j=b.length()-1;
         int carry=0;
