8f8e86e9243e96a9f48403e3b2395630f4dad454
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-08-06 16:50:56 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (73.55%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,7 +11,7 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        
+        /*
         Set<ListNode> set=new HashSet<>();
         ListNode curr=head;
         while(curr!=null){
@@ -22,5 +22,23 @@ public class Solution {
             curr=curr.next;
         }
         return false;
+        */
+
+        //Best Approach with fast and slow 2-pointers concept with time complexity O(n+k);
+        //Floyd hare and tortoise algorithm using the slow and fast 2-pointers
+
+        if(head==null){
+            return false;
+        }
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(slow!=fast){
+            if(fast==null || fast.next==null){
+                return false;
+            }            
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return true;
     }
 }
