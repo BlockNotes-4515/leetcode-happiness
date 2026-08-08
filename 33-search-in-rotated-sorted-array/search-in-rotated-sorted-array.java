710968393771de82566956c2952afc3b64fc8887
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-08-08 13:25:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (92.55%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -28,16 +28,16 @@ class Solution {
             if(nums[mid]==target){
                 return mid;
             }
-            //Searching in 'right' sorted portion of an array
+            //Searching in the 'right' portion of an sorted array
             else if(nums[mid]<=nums[right]){
                 if(nums[mid]<target && target<=nums[right]){
                     left=mid+1;
-                } 
+                }
                 else{
                     right=mid-1;
                 }
             }
-            //Searching in 'left' sorted portion of an array
+            //Searching in the 'left' portion of an sorted array
             else{
                 if(nums[left]<=target && target<nums[mid]){
                     right=mid-1;
@@ -46,7 +46,7 @@ class Solution {
                     left=mid+1;
                 }
             }
-        }
-        return -1;   
+        }  
+        return -1;
     }
 }
