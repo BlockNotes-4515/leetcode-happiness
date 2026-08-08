719b216249fe8c718adadb9609442d764a4f489b
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-08-08 13:19:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (65.92%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        /*Normal binary search in sorted manner
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return -1;
+        */
+        //Solving for rotated sorted array using binary search 
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }
+            //Searching in 'right' sorted portion of an array
+            else if(nums[mid]<=nums[right]){
+                if(nums[mid]<target && target<=nums[right]){
+                    left=mid+1;
+                } 
+                else{
+                    right=mid-1;
+                }
+            }
+            //Searching in 'left' sorted portion of an array
+            else{
+                if(nums[left]<=target && target<nums[mid]){
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1;
+                }
+            }
+        }
+        return -1;   
+    }
+}
