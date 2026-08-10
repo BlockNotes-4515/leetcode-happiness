051ed664f3ae1c9570456f942e69d053fdc5bdcc
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-08-10 11:29:33 +0530

    Time: 3 ms (87.64%) | Memory: 43.2 MB (87.13%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,24 +1,21 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        
+
         int n=heights.length;
-        int expec[]=new int[n];
+        int[] expec=new int[n];
         expec=heights.clone();
 
-        //Bubble sort an expected array
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n-i-1;j++)
-            {
-                if(expec[j]>expec[j+1])
-                {
+        //Sorting the expected array
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(expec[j]>expec[j+1]){
                     int temp=expec[j];
                     expec[j]=expec[j+1];
                     expec[j+1]=temp;
                 }
             }
         }
-        //Comparing the height original and expected array result
+        //Count the heights which are not met with expected array
         int count=0;
         for(int i=0;i<n;i++){
             if(heights[i]!=expec[i]){
