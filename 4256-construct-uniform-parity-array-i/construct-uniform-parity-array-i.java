71bfda6d0153ab37af0ee5958a89ea5cff90a50f
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-09-02 17:43:05 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (99.02%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        /*
+        int even=0;
+        int odd=0;
+
+        for(int x:nums1){
+            if(x%2==0){
+                even++;
+            }
+            else{
+                odd++;
+            }
+        }
+        if(even==0 || odd==0){
+            return nums1;
+        }
+        return new int[0];
+        */
+        return true;
+    }
+}
