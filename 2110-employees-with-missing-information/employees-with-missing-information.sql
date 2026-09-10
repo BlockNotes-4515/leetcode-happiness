f042a5eba0819d49b9e6c770c6683c4af96ef93c
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-09-10 18:52:26 +0530

    Time: 1288 ms (5.04%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+SELECT employee_id
+from Employees
+
+UNION
+
+SELECT employee_id
+from Salaries
+
+EXCEPT
+
+SELECT e.employee_id
+FROM Employees e
+JOIN Salaries s
+ON e.employee_id=s.employee_id
+ORDER BY employee_id;
+
+
+
+
