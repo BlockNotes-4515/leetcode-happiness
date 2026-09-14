e24efaa16d97bef53b2a92dd817e322639df4e74
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-09-14 16:25:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (17.64%) - LeetSync

--- 866-rectangle-overlap/rectangle-overlap.java
+++ 866-rectangle-overlap/rectangle-overlap.java
@@ -1,9 +1,12 @@
 class Solution {
     public boolean isRectangleOverlap(int[] rect1, int[] rect2) {
         
-        return rect1[0]<rect2[2] &&
-        rect1[2]>rect2[0] &&
-        rect1[1]<rect2[3] &&
-        rect1[3]>rect2[1];
+        int left=Math.max(rect1[0],rect2[0]);
+        int right=Math.min(rect1[2],rect2[2]);
+
+        int bottom=Math.max(rect1[1],rect2[1]);
+        int top=Math.min(rect1[3],rect2[3]);
+
+        return left<right && bottom<top;
     }
 }
