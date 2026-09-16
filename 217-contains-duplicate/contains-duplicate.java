9fb7e38999b6175fea745b044043cbaf5453e440
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-09-16 12:41:14 +0530

    Time: 17 ms (88.22%) | Memory: 108.3 MB (38.05%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -2,11 +2,23 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         
         //First we need to sort an array
+        //First Approach Brute Force Approach
+        /*
         Arrays.sort(nums);
         for(int i=1;i<nums.length;i++){
             if(nums[i]==nums[i-1]){
                 return true;
             }
+        }
+        return false;
+        */
+        HashSet<Integer> set=new HashSet<>();
+
+        for(int num:nums){
+            if(set.contains(num)){
+                return true;
+            }
+            set.add(num);
         }
         return false;
     }
