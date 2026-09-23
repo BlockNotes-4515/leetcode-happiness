bca30114dfc127ead0a1eb267736aeb662093d56
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-09-23 12:40:26 +0530

    Time: 1 ms (56.28%) | Memory: 48.8 MB (14.66%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -4,10 +4,9 @@ class Solution {
         int[] Pmax=PrefixMax(height);
         int[] Smax=SuffixMax(height);
         int ans=0;
-
         for(int i=1;i<n-1;i++){
             int lb=Pmax[i-1];
-            int rb=Smax[i+1];
+            int rb=Smax[i+1]; 
             int yb=Math.min(lb,rb);
             int amt=yb-height[i];
             if(amt>0){
@@ -16,28 +15,26 @@ class Solution {
         }
         return ans;
     }
-
-
-        static int[] PrefixMax(int[] arr){
-                int n=arr.length;
-                int[] Pmax=new int[n];
-                Pmax[0]=arr[0];
-                for(int i=1;i<n;i++){
-                    Pmax[i]=arr[i]>Pmax[i-1]
-                    ?arr[i]
-                    :Pmax[i-1];
-                }
-                return Pmax;
+    public int[] PrefixMax(int[] arr){
+        int n=arr.length;
+        int[] Pmax=new int[n];
+        Pmax[0]=arr[0];
+        for(int i=1;i<n;i++){
+            Pmax[i]=arr[i]>Pmax[i-1]
+            ?arr[i]
+            :Pmax[i-1];
         }
-        static int[] SuffixMax(int[] arr){
-                int n=arr.length;
-                int[] Smax=new int[n];
-                Smax[n-1]=arr[n-1];
-                for(int i=n-2;i>=0;i--){
-                    Smax[i]=arr[i]>Smax[i+1]
-                    ?arr[i]
-                    :Smax[i+1];
-                }
-                return Smax;
+        return Pmax;
+    }
+    public int[] SuffixMax(int[] arr){
+        int n=arr.length;
+        int[] Smax=new int[n];
+        Smax[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            Smax[i]=arr[i]>Smax[i+1]
+            ?arr[i]
+            :Smax[i+1];
         }
+        return Smax;
+    }
 }
