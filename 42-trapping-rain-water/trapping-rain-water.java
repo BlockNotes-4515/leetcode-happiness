efaae5c01083ef2b34704eb520a5b9e16f85f36e
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-09-23 12:25:25 +0530

    Time: 1 ms (56.28%) | Memory: 48.6 MB (20.92%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,27 +1,43 @@
 class Solution {
     public int trap(int[] height) {
+        int n=height.length;
+        int[] Pmax=PrefixMax(height);
+        int[] Smax=SuffixMax(height);
+        int ans=0;
 
-        //By, using the brute force approach
-        int water=0;
-        for(int i=1;i<height.length-1;i++){
-
-            int curr=height[i];
-
-            int leftMax=curr;
-            int rightMax=curr;
-
-            for(int j=i-1;j>=0;j--){
-                leftMax=Math.max(leftMax,height[j]);
+        for(int i=1;i<n-1;i++){
+            int lb=Pmax[i-1];
+            int rb=Smax[i+1];
+            int yb=Math.min(lb,rb);
+            int amt=yb-height[i];
+            if(amt>0){
+                ans=ans+amt;
             }
-            for(int j=i+1;j<height.length;j++){
-                rightMax=Math.max(rightMax,height[j]);
-
-                if(rightMax>=leftMax){
-                    break;
-                }
-            }
-            water=water+Math.min(leftMax,rightMax)-curr;
         }
-        return water;
+        return ans;
     }
+
+
+        static int[] PrefixMax(int[] arr){
+                int n=arr.length;
+                int[] Pmax=new int[n];
+                Pmax[0]=arr[0];
+                for(int i=1;i<n;i++){
+                    Pmax[i]=arr[i]>Pmax[i-1]
+                    ?arr[i]
+                    :Pmax[i-1];
+                }
+                return Pmax;
+        }
+        static int[] SuffixMax(int[] arr){
+                int n=arr.length;
+                int[] Smax=new int[n];
+                Smax[n-1]=arr[n-1];
+                for(int i=n-2;i>=0;i--){
+                    Smax[i]=arr[i]>Smax[i+1]
+                    ?arr[i]
+                    :Smax[i+1];
+                }
+                return Smax;
+        }
 }
