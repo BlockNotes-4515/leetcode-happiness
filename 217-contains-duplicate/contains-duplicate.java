bbcc5eb597d812ca496dbf727cb5e68c7e9f3040
Dhruv Dhayal_ <137479629+BlockNotes-4515@users.noreply.github.com>
2026-09-24 11:59:10 +0530

    Time: 17 ms (88.50%) | Memory: 108.1 MB (57.11%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -4,16 +4,17 @@ class Solution {
         //First we need to sort an array
         //First Approach Brute Force Approach
         /*
-        Arrays.sort(nums);
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]==nums[i-1]){
-                return true;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
             }
         }
         return false;
         */
         HashSet<Integer> set=new HashSet<>();
-
         for(int num:nums){
             if(set.contains(num)){
                 return true;
